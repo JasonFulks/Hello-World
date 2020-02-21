f52e0a3ed2fa5536f76bd8789f8585b01b511dc2
Jason Fulks <jason.fuks.2023@student.lhschools.org>
2020-02-21 11:53:46 -0500

    intellij commit

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,16 @@
+package com.LickingHeights;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.print("My name is ");
+        System.out.println("Jason");
+
+        //Print three more lines about what I did over winter break
+
+        System.out.println("Over winter break I had a lot of fun!");
+        System.out.println("I got towels and a bunch of bedding stuff for Christmas which is what I wanted.");
+        System.out.println("I also went to a retreat with friends and 13 people got sick. Including me! So that is why I wasn't here on Monday.");
+    }
+}
